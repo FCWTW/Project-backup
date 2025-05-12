a4af1e78a2a3e7330f4f050f2ed1eec42bdc5379
Wayne <waynetw1223@gmail.com>
2025-05-12 20:40:40 +0800

    little update

--- YOLO/pt2tflite.py
+++ YOLO/pt2tflite.py
@@ -1,19 +1,35 @@
 '''
 Source:
 https://docs.ultralytics.com/integrations/tflite/
-https://docs.ultralytics.com/zh/integrations/edge-tpu/
+https://github.com/tensorflow/probability/releases/tag/v0.21.0
+
+conda create -n yolo-tf python=3.9 -y
+pip install tensorflow==2.13.0 tensorflow-addons==0.22.0
+pip install tensorflow-probability==0.21.0
+pip install ultralytics
+pip install onnx onnx-tf
 '''
 
 from ultralytics import YOLO
+from onnx_tf.backend import prepare
+import onnx
+import tensorflow as tf
 
-# Load the YOLO11 model
+# .pt to .onnx
 model = YOLO("yolo11n.pt")
+model.export(format="onnx", opset=16)
 
-# Export the model to TFLite format
-model.export(format="tflite")  # creates 'yolo11n_float32.tflite'
+# .onnx to SavedModel
+onnx_model = onnx.load("yolo11n.onnx")
+tf_rep = prepare(onnx_model)
+tf_rep.export_graph("model_tf")
 
-# Load the exported TFLite model
-tflite_model = YOLO("yolo11n_float32.tflite")
+# SavedModel to .tflite
+converter = tf.lite.TFLiteConverter.from_saved_model("model_tf")
+tflite_model = converter.convert()
+with open("yolo11n.tflite", "wb") as f:
+    f.write(tflite_model)
 
-# Run inference
+# Test
+tflite_model = YOLO("yolo11n.tflite")
 results = tflite_model("https://ultralytics.com/images/bus.jpg")
